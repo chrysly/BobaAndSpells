0c159e57cfb11a9308be9b2fe74e7248df6a00a8
clee857 <chrysly0816@gmail.com>
2023-12-26 20:14:01 -0600

    Base file structure setup

---
+++ src/main/java/chrysly/degenrpg/Customs/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for custom implementation of in game objects that don't exist in Minecraft's API.
+ * Includes any boilerplate class for:
+ * - Custom Items
+ * - Custom Blocks
+ * - Custom Entities
+ * ...and more
+ */
+package chrysly.degenrpg.Customs;
